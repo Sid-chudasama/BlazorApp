1a73a05bf0773e1cbc15975d65f724cf73a52641
chudas_s <chudas_s@LCILN19600>
2023-03-03 11:57:02 -0500

    Add project files.

---
+++ Controller/ForecastController.cs
@@ -0,0 +1,21 @@
+﻿using BlazorApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForecastController : ControllerBase
+    {
+        private readonly WeatherForecastService _weatherForecastService;
+        public ForecastController(WeatherForecastService weatherForecastService) { 
+            _weatherForecastService = weatherForecastService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> GetForecastAsync()
+        {
+            return new OkObjectResult(await _weatherForecastService.GetForecastAsync(DateOnly.FromDateTime(DateTime.Now)));
+        }
+    }
+}
